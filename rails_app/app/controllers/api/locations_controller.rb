7de0036d6171359b91ce20ee76caddeefd97b9b5
Mahir Bansal <mb@mahirbansal.com>
2025-06-22 15:08:47 -0400

    worked on map

--- rails_app/app/controllers/api/locations_controller.rb
+++ rails_app/app/controllers/api/locations_controller.rb
@@ -1,9 +1,9 @@
 class Api::LocationsController < ApplicationController
   def index
-    render json: Location.all
+  render json: Location.all.as_json(methods: [:risk_score])
   end
 
   def show
-    render json: Location.find(params[:id])
+  render json: Location.find(params[:id]).as_json(methods: [:risk_score])
   end
 end
