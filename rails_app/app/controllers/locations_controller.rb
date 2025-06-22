26087907d39e7ec63f9fea044b9bc3cebabc9578
Mahir Bansal <137363533+mbansal2006@users.noreply.github.com>
2025-06-22 00:32:00 -0400

    Add basic map view and filters for Locations

--- rails_app/app/controllers/locations_controller.rb
+++ rails_app/app/controllers/locations_controller.rb
@@ -1,6 +1,22 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+
+    if params[:q].present?
+      query = "%#{params[:q]}%"
+      @locations = @locations.where("address LIKE ? OR country LIKE ? OR duns_number LIKE ?", query, query, query)
+    end
+
+    if params[:risk_bucket].present?
+      range = case params[:risk_bucket]
+              when 'low' then 0..33
+              when 'medium' then 34..66
+              when 'high' then 67..1000
+              end
+      @locations = @locations.where(risk_score: range) if range
+    end
+
+    @locations_json = @locations.select(:id, :latitude, :longitude, :country, :address, :duns_number, :risk_score).to_json
   end
 
   def show
