9dba9857562d7d9122c27f2287a9d9d16d464204
Arvin Dang <git@arvindang.com>
2025-07-01 21:17:04 -0500

    pg setup for fly

--- rails_app/config/initializers/cors.rb
+++ rails_app/config/initializers/cors.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow all localhost origins for development
-    origins '*'
+    if Rails.env.development?
+      # Allow all origins for development
+      origins '*'
+    else
+      # Allow specific origins for production
+      origins 'https://drug-monitor-frontend.fly.dev', /https:\/\/.*\.fly\.dev$/
+    end
     
     resource '*',
       headers: :any,
