aee14f1d9c43e393778dc7943bbb3680685fde5a
Mahir Bansal <mb@mahirbansal.com>
2025-07-01 21:41:03 -0400

    Prepare Rails for HTTPS via DuckDNS

--- rails_app/config/environments/production.rb
+++ rails_app/config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  config.hosts << "drugmonitor.duckdns.org"
+  
   config.enable_reloading = false
 
   # Eager load code on boot. This eager loads most of Rails and
